9798aa551065f8deab615612b09bfd41be983bc0
Sergey Lushkin <serg.nn1985@gmail.com>
2026-03-24 21:14:31 +0300

    Добавил lesson2

---
+++ nodejs/index.js
@@ -0,0 +1,21 @@
+// console.log('Hello, node.js!');
+
+
+// const _currentValue = true;
+// console.log(is_user_from_russia);
+
+// console.log(String(NaN));
+
+// console.log(Number('  5sfsdf    '))
+//console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(Number(false));
+
+// console.log(Boolean(NaN));
+
+// console.log(10 % 2);
+// console.log(2 ** 3);
+
+// console.log(+ "   55   ");
+
+console.log('Ivan' + true);
