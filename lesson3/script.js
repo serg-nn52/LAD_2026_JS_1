535953bbb9528b1136fa14fae15c09c8dd5bc658
Sergey Lushkin <serg.nn1985@gmail.com>
2026-03-31 21:26:41 +0300

    Добавил lesson3

---
+++ lesson3/script.js
@@ -0,0 +1,100 @@
+// console.log('test lesson3')
+
+// console.log('авто' == "авто");
+// console.log('авто' < "автомобиль");
+// console.log('а' < "я");
+// console.log('Я' > "а");
+
+// console.log(false === '0');
+// console.log(+null === 0);
+// console.log("  -9  " + 5);
+
+// const age = NaN;
+// console.log(isNaN(age));
+
+// if(2 < 1) {
+//     console.log('Первый блок');
+// } else if(5 < 1) {
+//     console.log("Второй блок");
+// } else {
+//     console.log("Блок else")
+// }
+// const age = 20;
+// const message = age < 18 ? "Доступ запрещен!" : "Доступ разрешен!";
+
+// let message;
+// if(age < 18) {
+//     message = "Доступ запрещен!"
+// } else {
+//     message = "Доступ разрешен!"
+// }
+// console.log(message);
+
+// const country = 'Russia';
+
+// switch(country) {
+//     case 'Russia':
+//         console.log('Русский язык');
+//         break;
+//     case 'Belarus':
+//         console.log('Русский или белорусский язык');
+//         break;
+//     case 'USA': 
+//         console.log("Английский язык");
+//         break;
+//     default:
+//         console.log('Язык неизвестен');
+// }
+
+// let line1 = 120;
+// let line2 = 1;
+// let line3 = 80;
+
+// let maxValue = line1;
+
+// if(line2 > maxValue) {
+//     maxValue = line2;
+// } 
+
+// if(line3 > maxValue) {
+//     maxValue = line3;
+// }
+// console.log(maxValue);
+// if (line1 > line2) {
+//   if (line1 > line3) {
+//     console.log(line1);
+//   } else {
+//     console.log(line3);
+//   }
+// } else {
+//   if (line2 > line3) {
+//     console.log(line2);
+//   } else {
+//     console.log(line3);
+//   }
+// }
+
+// let temperature = 25;
+
+// if (temperature <= -30) {
+//   console.log("Оставайтесь дома");
+// } else if (temperature <= -10) {
+//   console.log("Сегодня холодно");
+// } else if (temperature <= 5) {
+//   console.log("Не холодно");
+// } else if (temperature <= 15) {
+//   console.log("Тепло");
+// } else if (temperature <= 25) {
+//   console.log("Очень тепло");
+// } else if (temperature <= 35) {
+//   console.log("Жарко");
+// } else {
+//   console.log("Пекло");
+// }
+
+// console.log(null  ||  0  ||  ""  ||  undefined)
+// console.log('яблоко'  &&  true  &&  null  &&  1)
+// console.log(0  ||  true  &&  'false'  ||  null)
+// console.log(0  &&  true  ||  'false'  &&  null)
+// console.log(!0 && !!1)
+console.log(!(null || !'апельсин' && true))
